312788cf98f424b426a127ec5762b50a91d936df
Enock <kipkorirenock594@gmail.com>
2026-04-07 20:33:22 +0300

    Added next_prime.py file .

--- next_prime.py
+++ next_prime.py
@@ -15,14 +15,14 @@ def my_prime(num):
 def next_prime(n):
     next_num = n + 1
     while True :
-        if my_prime(next_num) :
-            return next_num 
+        if my_prime(next_num):
+            return next_num
         next_num += 1
 
 try:
     user_input = int(input("Enter a number to find the next prime number:\n"))
     next_prime_number = next_prime(user_input)
-    print(f"The next prime number after{user_input} is : {next_prime_number}")
+    print(f"The next prime number after {user_input} is : {next_prime_number}")
 
 except ValueError:
     print("Invalid input please enter an integer")
