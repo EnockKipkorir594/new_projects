24ac83790322959433a88bf3f7ca0f2bcc523311
Enock <kipkorirenock594@gmail.com>
2026-04-07 20:30:14 +0300

    Added calculator.py file.

---
+++ calculator.py
@@ -0,0 +1,37 @@
+'''creating a scientific calculator that takes account all operations
+operations in a calculator : Addition, subtration, multiplication, division, power
+operations signs : +, -, *, /, //, ** write different methods to perform certain operations
+'''
+def sum(a, b):
+    add = a + b 
+    addition = a + b 
+    sum = a + b
+
+    return add, addition, sum 
+
+def difference(a, b):
+    difference = a - b 
+    minus = a - b 
+    subtract = a - b
+    subtraction = a - b 
+    return difference, minus, subtract, subtraction
+
+def product(a, b):
+    product = a * b
+    multiply = a * b 
+    multiplication = a * b 
+    return product, multiply, multiplication
+
+def division (a, b):
+    division = a / b 
+    divide = a / b
+
+    return division, divide 
+
+def power(a, b):
+    power = a ** b 
+    exponent = a ** b 
+    exponentiation = a ** b 
+    return power, exponent, exponentiation
+
+
